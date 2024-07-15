486adfc04847e704210d0fa115caa2b8ac611028
Sebastian Schulz <ses.schulz+bitbucket@gmail.com>
2024-07-15 12:35:36 +0200

    Provide middlewares for validation

--- updater.go
+++ updater.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/subtle"
+	"net/http"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -18,4 +19,44 @@ type hashedPassword struct {
 func (p *hashedPassword) isValid(origPasswd []byte) bool {
 	key := argon2.IDKey(origPasswd, p.salt, p.time, p.memory, p.threads, p.keyLen)
 	return subtle.ConstantTimeCompare(key, p.key) == 1
+}
+
+func PasswordValidationMiddleware(validate func(origPasswd []byte) bool) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			passwd := r.URL.Query().Get("passwd")
+
+			if passwd == "" {
+				http.Error(w, "user or password wrong", http.StatusUnauthorized)
+				return
+			}
+
+			if !validate([]byte(passwd)) {
+				http.Error(w, "user or password wrong", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func UserValidationMiddleware(user string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			unverifiedUser := r.URL.Query().Get("user")
+
+			if unverifiedUser == "" {
+				http.Error(w, "user or password wrong", http.StatusUnauthorized)
+				return
+			}
+
+			if subtle.ConstantTimeCompare([]byte(user), []byte(unverifiedUser)) != 1 {
+				http.Error(w, "user or password wrong", http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
--- updater_test.go
+++ updater_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -8,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/httplog/v2"
 )
 
 func TestHashedPasswordValidation(t *testing.T) {
@@ -34,129 +35,176 @@ func TestHashedPasswordValidation(t *testing.T) {
 	}
 }
 
-type passwordValidator interface {
-	isValid(origPasswd []byte) bool
-}
-
-type updater struct {
-	user              string
-	passwordvalidator passwordValidator
-}
-
-// func newUpdater() http.Handler {
-// 	route := chi.NewRouter()
-
-// }
-
-func parseAddrOrEmpty(ipStr string, ipv6 bool) (netip.Addr, error) {
-	if ipStr == "" && !ipv6 {
-		return netip.MustParseAddr("0.0.0.0"), nil
-	}
-	if ipStr == "" && ipv6 {
-		return netip.MustParseAddr("::"), nil
-	}
-	return netip.ParseAddr(ipStr)
-}
-
-func (u *updater) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user := r.URL.Query().Get("user")
-	passwd := r.URL.Query().Get("passwd")
-
-	if user == "" || passwd == "" {
-		http.Error(w, "user or password wrong", http.StatusUnauthorized)
-		return
-	}
-
-	if !u.passwordvalidator.isValid([]byte(passwd)) {
-		http.Error(w, "user or password wrong", http.StatusUnauthorized)
-		return
-	}
-
-	ipaddr, err := parseAddrOrEmpty(r.URL.Query().Get("ipaddr"), false)
-	if err != nil {
-		http.Error(w, "ipaddr is incorrect", http.StatusBadRequest)
-	}
-	if !ipaddr.Is4() {
-		http.Error(w, "ipaddr is incorrect", http.StatusBadRequest)
-	}
-
-	ip6addr, err := parseAddrOrEmpty(r.URL.Query().Get("ip6addr"), true)
-	if err != nil {
-		http.Error(w, "ip6addr is incorrect", http.StatusBadRequest)
-		return
-	}
-	if !ip6addr.Is6() {
-		http.Error(w, "ip6addr is incorrect", http.StatusBadRequest)
-		return
-	}
-
-	log := httplog.LogEntry(r.Context())
-	log.Debug("IP Adress", ipaddr.String(), ip6addr)
-
-	fmt.Fprintln(w, "Ok")
-}
-
-type mockPasswordValidator struct {
-	called  uint
-	checker func(origPasswd []byte) bool
-}
-
-func (m *mockPasswordValidator) isValid(origPasswd []byte) bool {
-	m.called++
-	return m.checker(origPasswd)
-}
-
-func TestUpdaterHandler(t *testing.T) {
-
-	handler := updater{}
-	route := chi.NewRouter()
-	route.Mount("/", &handler)
+func TestPasswordValidationMiddleware(t *testing.T) {
 
 	for _, testCase := range []struct {
-		input                 *http.Request
-		mockPasswordValidator mockPasswordValidator
-		expectedStatusCode    int
+		input              *http.Request
+		validate           func(t *testing.T, origPasswd []byte) bool
+		expectedStatusCode int
 	}{
-		{httptest.NewRequest("GET", "/", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			t.FailNow()
-			return false
-		}}, 401},
-		{httptest.NewRequest("GET", "/?user=test&passwd=.test.", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			if string(origPasswd) != ".test." {
-				t.Errorf("unexpected password")
-			}
-			return true
-		}}, 200},
-		{httptest.NewRequest("GET", "/?user=test&passwd=.test.&ipaddr=a.168.1.1", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			if string(origPasswd) != ".test." {
-				t.Errorf("unexpected password")
+		{
+			httptest.NewRequest("GET", "/", nil),
+			func(t *testing.T, origPasswd []byte) bool {
+				t.Errorf("function should not be called")
 				return false
-			}
-			return true
-		}}, 400},
-		{httptest.NewRequest("GET", "/?user=test&passwd=.test.&ipaddr=192.168.1.1", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			if string(origPasswd) != ".test." {
-				t.Errorf("unexpected password")
+			},
+			401,
+		},
+		{
+			httptest.NewRequest("GET", "/?passwd=.test.", nil),
+			func(t *testing.T, origPasswd []byte) bool {
+				if !bytes.Equal(origPasswd, []byte(".test.")) {
+					t.Errorf("password missmatch: %s != \".test.\"", origPasswd)
+					return false
+				}
+				return true
+			},
+			200,
+		},
+		{
+			httptest.NewRequest("GET", "/?passwd=.test.", nil),
+			func(t *testing.T, origPasswd []byte) bool {
 				return false
-			}
-			return true
-		}}, 200},
-		{httptest.NewRequest("GET", "/?user=test&passwd=.test.&ip6addr=2001:db8::x", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			if string(origPasswd) != ".test." {
-				t.Errorf("unexpected password")
-				return false
-			}
-			return true
-		}}, 400},
-		{httptest.NewRequest("GET", "/?user=test&passwd=.test.&ip6addr=2001:db8::1", nil), mockPasswordValidator{checker: func(origPasswd []byte) bool {
-			if string(origPasswd) != ".test." {
-				t.Errorf("unexpected password")
-				return false
-			}
-			return true
-		}}, 200},
+			},
+			401,
+		},
 	} {
-		handler.passwordvalidator = &testCase.mockPasswordValidator
+		route := chi.NewRouter()
+		route.Use(PasswordValidationMiddleware(func(origPasswd []byte) bool {
+			return testCase.validate(t, origPasswd)
+		}))
+		route.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Ok")
+		})
+
+		w := httptest.NewRecorder()
+		route.ServeHTTP(w, testCase.input)
+		resp := w.Result()
+		if resp.StatusCode != testCase.expectedStatusCode {
+			t.Errorf("status code is %v instead of %v", resp.StatusCode, testCase.expectedStatusCode)
+		}
+	}
+}
+
+func TestUserValidationMiddleware(t *testing.T) {
+
+	for _, testCase := range []struct {
+		input              *http.Request
+		expectedStatusCode int
+	}{
+		{httptest.NewRequest("GET", "/", nil), 401},
+		{httptest.NewRequest("GET", "/?user=", nil), 401},
+		{httptest.NewRequest("GET", "/?user=foobar", nil), 401},
+		{httptest.NewRequest("GET", "/?user=baz", nil), 200},
+	} {
+		route := chi.NewRouter()
+		route.Use(UserValidationMiddleware("baz"))
+		route.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Ok")
+		})
+
+		w := httptest.NewRecorder()
+		route.ServeHTTP(w, testCase.input)
+		resp := w.Result()
+		if resp.StatusCode != testCase.expectedStatusCode {
+			t.Errorf("status code is %v instead of %v", resp.StatusCode, testCase.expectedStatusCode)
+		}
+	}
+}
+
+type ctxIPKey struct{ uint8 }
+
+var ctxIPv4Key = ctxIPKey{uint8: 0}
+var ctxIPv6Key = ctxIPKey{uint8: 1}
+
+func IPValidationMiddleware(next http.Handler) http.Handler {
+	parseAddrOrEmpty := func(ipStr string) (*netip.Addr, error) {
+		if ipStr == "" {
+			return nil, nil
+		}
+		r, err := netip.ParseAddr(ipStr)
+		if err != nil {
+			return nil, err
+		}
+		return &r, nil
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		valid := true
+
+		ipaddr, err := parseAddrOrEmpty(r.URL.Query().Get("ipaddr"))
+		if err != nil {
+			valid = false
+			http.Error(w, "ipaddr is incorrect", http.StatusBadRequest)
+		}
+		if ipaddr != nil && !ipaddr.Is4() {
+			valid = false
+			http.Error(w, "ipaddr is incorrect", http.StatusBadRequest)
+		}
+
+		ip6addr, err := parseAddrOrEmpty(r.URL.Query().Get("ip6addr"))
+		if err != nil {
+			valid = false
+			http.Error(w, "ip6addr is incorrect", http.StatusBadRequest)
+		}
+		if ip6addr != nil && !ip6addr.Is6() {
+			valid = false
+			http.Error(w, "ip6addr is incorrect", http.StatusBadRequest)
+		}
+
+		if valid && ipaddr != nil {
+			ctx = context.WithValue(ctx, ctxIPv4Key, ipaddr)
+		}
+		if valid && ip6addr != nil {
+			ctx = context.WithValue(ctx, ctxIPv6Key, ip6addr)
+		}
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func TestIPValidationMiddleware(t *testing.T) {
+	checkContext := func(r *http.Request, ctxKey ctxIPKey, expectedValue *netip.Addr) {
+		got, ok := r.Context().Value(ctxKey).(*netip.Addr)
+		if expectedValue != nil && !ok {
+			t.Errorf("expected value but got nothing")
+		}
+		if expectedValue == nil && ok {
+			t.Errorf("expected nothing but got %v", got)
+		}
+
+		if expectedValue != nil && *expectedValue != *got {
+			t.Errorf("expected %v but got: %v", expectedValue, got)
+		}
+	}
+
+	ipv4 := netip.MustParseAddr("192.168.1.1")
+	ipv6 := netip.MustParseAddr("2001:db8::1")
+
+	for _, testCase := range []struct {
+		input              *http.Request
+		expectedStatusCode int
+		expectedIPv4       *netip.Addr
+		expectedIPv6       *netip.Addr
+	}{
+		{httptest.NewRequest("GET", "/", nil), 200, nil, nil},
+		{httptest.NewRequest("GET", "/?ipaddr=192.168.1.1", nil), 200, &ipv4, nil},
+		{httptest.NewRequest("GET", "/?ip6addr=2001:db8::1", nil), 200, nil, &ipv6},
+		{httptest.NewRequest("GET", "/?ipaddr=192.168.1.1&ip6addr=2001:db8::1", nil), 200, &ipv4, &ipv6},
+
+		{httptest.NewRequest("GET", "/?ipaddr=a.168.1.1", nil), 400, nil, nil},
+		{httptest.NewRequest("GET", "/?ip6addr=2001:db8::x", nil), 400, nil, nil},
+		{httptest.NewRequest("GET", "/?ipaddr=a.168.1.1&ip6addr=2001:db8::1", nil), 400, nil, nil},
+		{httptest.NewRequest("GET", "/?ipaddr=192.168.1.1&ip6addr=2001:db8::x", nil), 400, nil, nil},
+	} {
+		route := chi.NewRouter()
+		route.Use(IPValidationMiddleware)
+		route.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			checkContext(r, ctxIPv4Key, testCase.expectedIPv4)
+			checkContext(r, ctxIPv6Key, testCase.expectedIPv6)
+
+		})
 
 		w := httptest.NewRecorder()
 		route.ServeHTTP(w, testCase.input)
